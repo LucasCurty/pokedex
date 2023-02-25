9a2f46ca76ab6ead12275671014207df872a57d3
Lucas Curty <lukas.curty.21@gmail.com>
2023-02-24 21:52:24 -0300

    [Style] Add style sheet

--- src/assets/pages/Pokemon.tsx
+++ src/assets/pages/Pokemon.tsx
@@ -3,27 +3,27 @@ import { useParams, Link } from "react-router-dom";
 import {PokemonStats} from '../types/typePokemon'
 
 function Pokemon(){
-  const [pokemon, setPokemon] = useState<PokemonStats| null>(null) ;
-  const {name} = useParams();
+  const [pokemon, setPokemon] = useState<PokemonStats | null>(null) ;
+  const {name} = useParams<any>();
 
   useEffect(()=>{
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    .then((res)=>res.json())
-    .then((resObj)=>setPokemon(resObj))
+      .then((res)=>res.json())
+      .then((resObj)=>setPokemon(resObj))
     },[name]);
 {console.log(pokemon)}
     return(
       <section className="pokemon_Section">
         <main className="pokemon">
           <div className="face">
-            <h1 id="title" className={pokemon?.types[0].type.name + ' animate-pulse'}>{pokemon?.name}</h1>
+            <h1 id="title" className={pokemon?.types[0].type.name}>{pokemon?.name}</h1>
             <img src={pokemon?.sprites.other["official-artwork"].front_default} 
               alt="Pokemon imagem" 
               className="w-64 m-auto "  
               />
           </div>
           <ul className="habilidades">
-            <p className=" font-bold">Habilidades</p>
+            <p className=" font-bold">Habilities</p>
             {pokemon?.abilities.map(item => <li>{item.ability.name}</li>)}
             
             <p className="pt-3 font-bold">Type</p>
@@ -44,9 +44,11 @@ function Pokemon(){
             }
           </ul>
           <div className="div_bottons">
-            <Link to="/" className="back_to_menu">Back</Link>
-            <Link to='/' className="previous_pokemon">Before Pokemon</Link>
-            <Link to="/" className="next_pokemon">Next Pokemon</Link>
+            <Link to="/" className="all_btn back_to_menu">Back Menu</Link>
+            <Link to={`/pokemon/${(Number(name)) === 1 ? 1 : Number(name) - 1}`} 
+            className={`all_btn previous_pokemon ${(Number(name) === 1 ? 'opacity-5 cursor-no-drop' : 'opacity-100')}`}>Before Pokemon</Link>
+            <Link to={`/pokemon/${(Number(name)) === 1008 ? 1008 : Number(name) + 1}`}  
+            className={`all_btn next_pokemon ${(Number(name) === 1008 ? 'opacity-5 cursor-no-drop' : 'opacity-100')}`}>Next Pokemon</Link>
           </div>
         </main>
       </section>
